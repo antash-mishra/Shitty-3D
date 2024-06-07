cd63aa962ad87406df61e18c087e3f1146064074
antash <mishraantash34@gmail.com>
2024-06-07 22:48:41 +0530

    lights added to physics

--- BasicPhysics/src/script.js
+++ BasicPhysics/src/script.js
@@ -3,10 +3,13 @@ import './style.css'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js'
 import GUI from 'lil-gui'
+import CANNON from 'cannon'
 
 
 const gui = new GUI()
 
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -17,20 +20,21 @@ const sizes = {
 
 const scene = new THREE.Scene()
 
-const planeGeometry = new THREE.PlaneGeometry(5, 5);
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
 const planeMaterial = new THREE.MeshStandardMaterial( {side: THREE.DoubleSide, roughness:0.4} );
 const planeMesh = new THREE.Mesh( planeGeometry, planeMaterial );
 
 planeMaterial.color = new THREE.Color('#4E4D4B')
 
-planeMesh.position.y = -0.65;
 planeMesh.rotation.x = Math.PI/2
+planeMaterial.roughness  = 0.4
+planeMaterial.metalness = 0.3
 planeMesh.receiveShadow = true
 
 scene.add( planeMesh );
 
 
-const sphereGeometry = new THREE.SphereGeometry(1,32,16)
+const sphereGeometry = new THREE.SphereGeometry(0.5,32,16)
 const sphereMaterial = new THREE.MeshStandardMaterial()
 
 const sphereMesh = new THREE.Mesh(
@@ -39,17 +43,33 @@ const sphereMesh = new THREE.Mesh(
 )
 
 sphereMaterial.roughness  = 0.4
-sphereMesh.position.y = 0.5
+sphereMaterial.metalness = 0.3
+sphereMesh.position.set(3,0.5, -3)
 sphereMesh.castShadow = true
 scene.add(sphereMesh)
 
 // Light
-const light = new THREE.AmbientLight( 0xffffff, 2  );
+const light = new THREE.AmbientLight( 0xffffff, 2.1  );
 scene.add(light);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+directionalLightCameraHelper.visible = true
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
-camera.position.z = 6
+camera.position.set(- 3, 3, 3)
 scene.add(camera)
 
 
@@ -65,8 +85,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 // Shadow Configuration
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.shadowMap.enabled = false
 
 renderer.setSize(sizes.width, sizes.height)
 
